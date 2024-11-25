aba4df0a1b916dd75cfb36fcfdc96b95bff2a4a9
Jonathan Wright <jdwright@ldc.upenn.edu>
2024-11-25 17:02:53 -0500

    combine.rb

--- bin/combine.rb
+++ bin/combine.rb
@@ -1,48 +1,45 @@
 #!/usr/bin/env ruby
+=begin
 
-fns = []
-out = nil
-header = nil
-ARGV.sort.each do |fn|
-  case fn
-  when /^o:/
-    out = fn[2..-1]
-  when /^h:/
-    header = fn.split(':')[1..-1]
-  else
-    fns << fn
-  end
+combine (concatenates) tsv files, check headers/columns, write to stdout
+
+    combine.rb file1 file2 ...
+
+The header can be specified with the first arg like this
+
+    combine.rb h:beg:end:text file1 file2 ...
+
+which indicates a three column format of beg, end, and text columns.
+
+If the header isn't given, the first file must have a header.  Otherwise headers are optional,
+and are discarded.  The output has a single header.
+
+=end
+
+require_relative '../lib/models'
+
+if ARGV[0] =~ /^h:(.+)/
+  header = $1.gsub ":", "\t"
+  fns = ARGV[1..-1]
+else
+  fns = ARGV
 end
 
-def check_header(header)
-  case header
-  when %w[ file beg end text speaker ]
-    :ok
-  else
-    :bad
-  end
-end
-
-if check_header(header) != :ok
-  raise 'error'
-end
-
-def get(fn, header)
-  lines = File.readlines(fn)
-  lines.each do |x|
-    a = x.split "\t"
-    if a.length != header.length
-      raise "bad line"
+sample = nil
+fns.sort.each do |fn|
+  string = File.read fn
+  if sample.nil?
+    sample = if header
+      Sample.new fn: fn, header: header
+    else
+      Sample.new fn: fn, string: string
     end
+  else
+    sample.add_from_string fn:, string:
   end
-  lines
 end
 
-lines = fns.map { |x| get x, header }.flatten
-#puts header
-#puts lines
-open(out, 'w') do |fn|
-  fn.puts header.join "\t"
-  fn.puts lines
-end
+sample.puts
+
+
 
---
+++ lib/models.rb
@@ -0,0 +1,115 @@
+require 'json'
+
+# a uniform set of segments
+class Sample
+
+  def initialize(fn:, string:, header:)
+    @segments = []
+    if header
+      check_header_line header
+    elsif string # try to identify format
+      case string
+      when /^\w/ # assume tsv
+        string.lines.map(&:chomp).each_with_index do |line, i|
+          if i == 0
+            check_header_line line
+          else
+            add_segment_from_line fn:, line:
+          end
+        end
+      when /^\s*{/ # assume json
+        add_object fn:, object: JSON.parse(string)
+      else
+        raise "unknown format"
+      end
+    end
+  end
+
+  def check_header_line(line)
+    @header_array = check_header line
+    if @header_array == :bad
+      raise "bad header: #{line}"
+    else
+      @header_string = line
+    end
+  end
+
+  # only /^file\tbeg\tend\ttext\speaker$/
+  def check_header(header)
+    case header
+    when /^file\tbeg\tend\ttext\tspeaker$/
+      header.split "\t"
+    when "start\tend\ttext"
+      @whisper = true
+      @header_string = "file\t#{header}"
+      @header_string.split "\t"
+    else
+      :bad
+    end
+  end
+
+  # add while checking format
+  def add_from_string(fn:, string:)
+    string.lines.map(&:chomp).each_with_index do |line, i|
+      if i == 0
+        raise "bad header: #{line}" if line != @header_string
+      else
+        add_segment_from_line fn:, line:
+      end
+    end
+  end
+
+  def add_segment_from_line(fn:, line:)
+    a = line.split "\t"
+    if a.length != @header_array.length
+      raise "bad line: #{line}"
+    end
+    segment = {}
+    if @whisper
+      segment[:file] = fn
+    end
+    @header_array.zip(a).each do |k, v|
+      v = v.to_f / 60 if @whisper and (k == 'beg' or k == 'end')
+      segment[k.to_sym] = v
+    end
+    @segments << segment
+  end
+
+
+  def norm(x)
+    x.gsub(/[-=#,.?()]/, '').downcase
+  end
+
+  def add_object(fn:, object:)
+    if object['monologues'] # rev
+      @header_array = %w[ file beg end text speaker ]
+      @header_string = @header_array.join "\t"
+      # @header ||= %w[ file beg end text speaker ]
+      object['monologues'].each do |m|
+        speaker = m['speaker']
+        m['elements'].each do |e|
+          s = {}
+          if e['type'] == 'text'
+            s = {
+              file: fn,
+              beg: e['ts'],
+              end: e['end_ts'],
+              text: e['value'],
+              speaker: speaker
+            }
+            @segments << s
+          end
+        end#.flatten.map { |x| norm x }
+      end
+    else
+      raise "unknown format"
+    end
+  end
+
+  def puts
+    puts @header_string
+    puts @segments.map { |x| @header_array.map { |y| x[y.to_sym] }.join "\t" }
+  end
+
+end
+
